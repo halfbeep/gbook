f2a82c00b805f3438177890ff085b1c799e4879a
Shaun Cains <shaun.cains@acsye.com>
2018-06-21 22:17:40 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -9,8 +9,10 @@ let options = {
     method: 'GET'
 }
 
+getBooksAndReview(runGbook, null, descp);
+
 // pseudo code to test with sort, extract & formatting code below
-function getAllBookPages(callback, bookCount, title) {
+function getBooksAndReview(callback, bookCount, title) {
 
     bkCount = bookCount || 0;
 
@@ -42,13 +44,24 @@ function getAllBookPages(callback, bookCount, title) {
 
                         for (let b = 0; b < booksFound; b++) {
 
+                            /*
+                            if(compareCostPerPage(cheapest5[0], booksFound[b]) >= 0) {
+
+                                // requires type checking see ReadMe
+                                cheapest5[0] = booksFound[b];
+
+                                cheapest5.sort(compareCostPerPage);
+
+                            }
+                            */ //
+
                             let individualBookCount = bkCount + b;
 
                             console.log('Bk nr: ' + individualBookCount)
 
                             console.log('Title: ' + parsedData['items'][b].volumeInfo.title)
 
-                            console.log('UID  : ' + parsedData['items'][b].id + ' AverageRating:  ' + parsedData['items'][b].volumeInfo.averageRating + '\n')
+                            // console.log('UID  : ' + parsedData['items'][b].id + ' AverageRating:  ' + parsedData['items'][b].volumeInfo.averageRating + '\n')
                         }
                         bkCount += booksFound;
 
@@ -59,7 +72,7 @@ function getAllBookPages(callback, bookCount, title) {
                     }
                 });
 
-                getAllBookPages(runGbook, bkCount, descp);
+                getBooksAndReview(runGbook, bkCount, descp);
 
             } else {
 
@@ -80,7 +93,41 @@ function runGbook(data) {
 
     console.log('Total books found ' + data);
 
+    // TODO; here dump print both arrays here with format as suggested
+
+
 }
 
-getAllBookPages(runGbook, null, descp);
 
+
+
+
+// utility finctions below:--
+//
+
+function compareCostPerPage (a, b) {
+    if (cpp(a) < cpp(b))
+        return -1;
+    if (cpp(a) > cpp(b))
+        return 1;
+    return 0;
+}
+
+// undefined check
+function cpp(book) {
+    //   if (book && book.saleInfo.listPrice.amount && book.volumeInfo.pageCount)
+    //      return book.saleInfo.listPrice.amount / book.volumeInfo.pageCount;
+    return Number.MAX_SAFE_INTEGER
+}
+
+function compareRating(a, b) {
+
+// more undefined check
+//    if(a.volumeInfo.averageRating && b.volumeInfo.averageRating) {
+    if (a.volumeInfo.averageRating < b.volumeInfo.averageRating)
+        return -1;
+    if (a.volumeInfo.averageRating > b.volumeInfo.averageRating)
+        return 1;
+    return 0;
+
+}
